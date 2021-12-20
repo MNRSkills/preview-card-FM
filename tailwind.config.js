816d366d66269ffe73eecf660e6515d864cbd0c6
MNR_Dev <mnrdev510@gmail.com>
2021-12-19 21:47:31 -0800

    costomized styles in config file

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "main-bg": "hsl(217, 54%, 11%)",
+      "card-bg": "hsl(216, 50%, 16%)",
+      line: "hsl(215, 32%, 27%)",
+      white: "hsl(0, 0%, 100%)",
+      soft_blue: "hsl(215, 51%, 70%)",
+      cyan: "hsl(178, 100%, 50%)",
+    },
+    extend: {
+      fontWeight:{
+        light: "300",
+        midium:"400",
+        heavy: "600",
+      }
+    },
+  },
+  plugins: [],
+};
